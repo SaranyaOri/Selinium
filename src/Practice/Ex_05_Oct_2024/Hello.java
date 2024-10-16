2582e1d42da717df10c12127e2deaf63bb66bcaa
SARANYA <saranya.cs07@gmail.com>
2024-10-16 22:01:03 +0530

    Initial commit for this project

--- src/Practice/Ex_05_Oct_2024/Hello.java
+++ src/Practice/Ex_05_Oct_2024/Hello.java
@@ -1,7 +1,8 @@
-package Practice.Ex_04_Oct_2024;
+package Practice.Ex_05_Oct_2024;
 
 public class Hello {
     public static void main(String[] args) {
         System.out.println("My first Try");
+
     }
 }

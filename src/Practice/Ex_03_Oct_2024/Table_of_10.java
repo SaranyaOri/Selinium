ae89d226d029eabb0d45df01dc306f80c05fa0a1
SARANYA <saranya.cs07@gmail.com>
2024-10-16 18:49:19 +0530

    Initial commit for this project

---
+++ src/Practice/Ex_03_Oct_2024/Table_of_10.java
@@ -0,0 +1,16 @@
+package Practice.Ex_03_Oct_2024;
+
+public class Table_of_10 {
+    public static void main(String[] args) {
+        System.out.println("10 x 1 = 10");
+        System.out.println("10 x 2 = 20");
+        System.out.println("10 x 3 = 30");
+        System.out.println("10 x 4 = 40");
+        System.out.println("10 x 5 = 50");
+        System.out.println("10 x 6 = 60");
+        System.out.println("10 x 7 = 70");
+        System.out.println("10 x 8 = 80");
+        System.out.println("10 x 9 = 90");
+        System.out.println("10 x 10 = 100");
+    }
+}
